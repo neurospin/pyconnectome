2f0dcc49596460fcfe7a2923bb74feaec3d0090a
Antoine Grigis <antoine.grigis@cea.fr>
2018-05-02 09:57:43 +0200

    pyconnectome/utils/filetools: return the reorientation matrix.

--- pyconnectome/utils/filetools.py
+++ pyconnectome/utils/filetools.py
@@ -1,5 +1,5 @@
 ##########################################################################
-# NSAP - Copyright (C) CEA, 2013
+# NSAP - Copyright (C) CEA, 2013 - 2018
 # Distributed under the terms of the CeCILL-B license, as published by
 # the CEA-CNRS-INRIA. Refer to the LICENSE file or to
 # http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
@@ -20,6 +20,7 @@ import shutil
 # Package import
 from pyconnectome import DEFAULT_FSL_PATH
 from pyconnectome.wrapper import FSLWrapper
+from pyconnectome.utils.regtools import flirt2aff
 
 # Third party
 import numpy
@@ -428,19 +429,29 @@ def fslreorient2std(input_image, output_image, fslconfig=DEFAULT_FSL_PATH):
     fslconfig: str (optional, default DEFAULT_FSL_PATH)
         The FSL configuration batch.
     """
-    # check the input parameter
+    # Check the input parameter
     if not os.path.isfile(input_image):
         raise ValueError("'{0}' is not a valid input file.".format(
                          input_image))
+    if not output_image.endswith(".nii.gz"):
+        output_image += ".nii.gz"
 
-    # Define the FSL command
-    cmd = ["fslreorient2std", input_image, output_image]
+    # Define the FSL commands
+    cmd1 = ["fslreorient2std", input_image, output_image]
+    cmd2 = ["fslreorient2std", input_image]
 
     # Call fslreorient2std
-    fslprocess = FSLWrapper(cmd, shfile=fslconfig)
+    fslprocess = FSLWrapper(cmd1, shfile=fslconfig)
     fslprocess()
+    fslprocess = FSLWrapper(cmd2, shfile=fslconfig)
+    fslprocess()
+    fsl_trf_file = output_image + ".fsl.trf"
+    with open(fsl_trf_file, "wt") as open_file:
+        open_file.write(fslprocess.stdout)
+    trf_file = output_image + ".trf"
+    numpy.savetxt(trf_file, flirt2aff(fsl_trf_file, output_image, input_image))
 
-    return glob.glob(output_image + "*")[0]
+    return output_image
 
 
 def surf2surf(input_surf, output_surf, fslconfig=DEFAULT_FSL_PATH):
