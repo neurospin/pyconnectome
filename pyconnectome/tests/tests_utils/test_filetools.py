5ad41e4670c422d7ce7c4e1be7be92aad30fdef9
Antoine Grigis <antoine.grigis@cea.fr>
2018-05-04 14:19:30 +0200

    pyconnectome/tests/tests_utils/test_filetool: pep8.

--- pyconnectome/tests/tests_utils/test_filetools.py
+++ pyconnectome/tests/tests_utils/test_filetools.py
@@ -77,7 +77,8 @@ class Fslreorient2std(unittest.TestCase):
     @mock.patch("pyconnectome.utils.filetools.flirt2aff")
     @mock.patch("pyconnectome.utils.filetools.glob.glob")
     @mock.patch("pyconnectome.utils.filetools.os.path.isfile")
-    def test_normal_execution(self, mock_isfile, mock_glob, mock_aff, mock_open):
+    def test_normal_execution(self, mock_isfile, mock_glob, mock_aff,
+                              mock_open):
         """ Test the normal behaviour of the function.
         """
         # Set the mocked function returned values.
